53010730bbf3ddd9b0d929262ccc045c1c8a42f3
Babak_Jahanshahi <b.jahanshahi@gmail.com>
2019-05-05 11:23:38 +0100

    Update
    Update CatchMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
+
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(z = matrix()) {
+  
+  
+  n <- NULL
+  set <- function(y) {
+    z <<- y
+    n <<- NULL
+  }
+  get <- function() z
+  setsolve <- function(solve) n <<- solve
+  getsolve <- function() n
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this function! 
+##I will add my command next time I start working on this!! Babak
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  n <- z$getsolve()
+  if(!is.null(n)) {
+    message("getting cached data")
+    return(n)
+  }
+  data <- z$get()
+  n <- solve(data, ...)
+  z$setsolve(n)
+  n
+}      ## Return a matrix that is the inverse of 'x'
